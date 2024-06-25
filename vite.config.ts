3f6dde374c313c5782e7f7032e99034326ae14ac
Kjiw0n <rwd0904@naver.com>
2024-06-25 15:49:11 +0900

    chore: 설정 파일에서 styled-component 외부 모듈 명시

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import svgr from "vite-plugin-svgr";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,4 +13,9 @@ export default defineConfig({
       },
     }),
   ],
+  build: {
+    rollupOptions: {
+      external: ['styled-components'],
+    },
+  },
 });
