4bc7642370267ec77708a905ff35307351c0260a
Kjiw0n <rwd0904@naver.com>
2024-06-26 18:35:53 +0900

    hotfix: 수정사항 미반영 관련 오류 수정

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_BASE_URL,
+        target: process.env.VITE_BASE_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
