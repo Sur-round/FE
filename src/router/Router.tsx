3e06f8d81a6dfdf686251e7a1491280cb30e1afd
Kjiw0n <rwd0904@naver.com>
2024-06-26 12:47:48 +0900

    feat: 헤더 layout 구성 및 라우터 수정

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -5,27 +5,34 @@ import ContactUsPage from '../pages/ContactUsPage/ContactUsPage';
 import HomePage from '../pages/HomePage/HomePage';
 import SurveyDistributionPage from '../pages/SurveyDistributionPage/SurveyDistributionPage';
 import SurveyListPage from '../pages/SurveyListPage/SurveyListPage';
+import RootLayout from '../layout';
 
 export const router = createBrowserRouter([
   {
-    path: PATH.HOME,
-    element: <HomePage />,
-  },
-  {
-    path: PATH.BRAND_STORY,
-    element: <BrandStoryPage />,
-  },
-  {
-    path: PATH.CONTACT_US,
-    element: <ContactUsPage />,
-  },
-  {
-    path: PATH.SURVEY_DISTRIBUTION,
-    element: <SurveyDistributionPage />,
-  },
-  {
-    path: PATH.SURVEY_LIST,
-    element: <SurveyListPage />,
+    path: '/',
+    element: <RootLayout />, // RootLayout을 루트 요소로 설정
+    children: [
+      {
+        path: PATH.HOME,
+        element: <HomePage />,
+      },
+      {
+        path: PATH.BRAND_STORY,
+        element: <BrandStoryPage />,
+      },
+      {
+        path: PATH.CONTACT_US,
+        element: <ContactUsPage />,
+      },
+      {
+        path: PATH.SURVEY_DISTRIBUTION,
+        element: <SurveyDistributionPage />,
+      },
+      {
+        path: PATH.SURVEY_LIST,
+        element: <SurveyListPage />,
+      },
+    ],
   },
 ]);
 
