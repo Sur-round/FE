d040e561337590a9438dda6dcb832a6ebcb7c94f
Kjiw0n <rwd0904@naver.com>
2024-06-26 12:37:08 +0900

    chore: page 구분 위한 텍스트 추가

--- src/pages/SurveyDistributionPage/SurveyDistributionPage.tsx
+++ src/pages/SurveyDistributionPage/SurveyDistributionPage.tsx
@@ -1,7 +1,7 @@
 interface SurveyDistributionPageProps {}
 
- const SurveyDistributionPage = ({}: SurveyDistributionPageProps) => {
-   return (<div></div>);
- };
+const SurveyDistributionPage = ({}: SurveyDistributionPageProps) => {
+  return <div>설문 배포</div>;
+};
 
- export default SurveyDistributionPage; 
+export default SurveyDistributionPage;
