afeab4f0ecfdc7a6580437e7821036ecb6712d7b
Kjiw0n <rwd0904@naver.com>
2024-06-27 13:46:13 +0900

    fix: 배포 시 api 엔드포인트 오류 수정

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const createInterceptor = () => {
   const baseInstance = axios.create({
     // baseURL: import.meta.env.BASE_URL,
-    baseURL: '/api',
+    baseURL: import.meta.env.VITE_BASE_URL || '/api',
   });
   baseInstance.defaults.withCredentials = true;
 
